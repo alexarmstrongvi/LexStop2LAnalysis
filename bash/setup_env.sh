63a3a73c40977c6e187c821c2374be02a13eae9c
Alex Armstrong <armstro1@uci.edu>
2019-07-04 06:09:34 -0700

    Split setup scripts (part 2)

--- bash/setup_env.sh
+++ bash/setup_env.sh
@@ -49,43 +49,9 @@ function main() {
         echo "WARNING :: build files not generated yet"
     fi
     
-
-    echo -e "\n\n"
-    echo "Getting git summary"
-    git_record=
-    #update_git_record "RestFrames" in detached head state
-    update_git_record "jigsawcalculator"
-    update_git_record "susynt-submit"
-    update_git_record "SusyNtCutflowLooper"
-    update_git_record "superflow"
-    #update_git_record "athena" in detached head state
-    update_git_record "SusyCommon"
-    update_git_record "SusyNtuple"
-    update_git_record "LexStop2LAnalysis"
-
-    echo -e "\nGit status summary"
-    echo -e "\t- package (branch) : commits ahead and behind"
-    echo -e $git_record
-    
     echo ""
     echo "Environment is setup"
     echo ""
     return 0
 }
-
-function get_branch() {
-    echo "$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')"
-}
-function get_diff_summary() {
-    echo "$(git rev-list --left-right --count "$(get_branch)"...origin/"$(get_branch)")"
-}
-function update_git_record() {
-    package=${1}
-    cd source/${package}
-    echo "Syncing ${package}..."
-    git fetch origin
-    git_record="\t- ${package} ($(get_branch)) : $(get_diff_summary)\n${git_record}"
-    cd ../..
-}
-
 main
